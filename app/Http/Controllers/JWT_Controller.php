6a32b7e0f512b150813c5badb45584dd00af8d96
Zuka_ <kid00345@hotmail.com>
2022-10-14 17:16:33 +0700

    Update MT_Master

--- app/Http/Controllers/JWT_Controller.php
+++ app/Http/Controllers/JWT_Controller.php
@@ -21,11 +21,11 @@ class JWT_Controller extends BaseController
 
             $data = $request->all();
 
-            if(!isset($data['username'])) throw new Exception('Required Parameter [username]');
-            if(!isset($data['password'])) throw new Exception('Required Parameter [password]');
+            if(!isset($data['username'])) throw new Exception('Required Parameter [username]', 1000);
+            if(!isset($data['password'])) throw new Exception('Required Parameter [password]', 1000);
 
             if($data['username'] != "api_ufund" || $data['password'] != "U6undp0Rt4l"){
-                throw new Exception('Invalid Username or Password');
+                throw new Exception('Invalid Username or Password', 2000);
             }
 
             date_default_timezone_set("Asia/Bangkok");
@@ -52,16 +52,37 @@ class JWT_Controller extends BaseController
             
 
             return response()->json(array(
-                'Code' => '9999',
+                'Code' => '0000',
                 'status' => 'Sucsess', 
                 'data' => $data_token,
             ));
 
         } catch (Exception $e) {
+
+            $MsgError = [
+                "1000" => [
+                    'status' => 'Invalid Data',
+                ],
+                "2000" => [
+                    'status' => 'Invalid Condition',
+                ],
+                "9000" => [
+                    'status' => 'System Error',
+                ],
+            ];
+
+            if ($e->getPrevious() != null) {
+                return response()->json(array(
+                    'Code' => '9000',
+                    'status' => 'System Error',
+                    'message' => $e->getPrevious()->getMessage(),
+                ));
+            }
+
             return response()->json(array(
-                'Code' => '00X2',
-                'status' => 'Error', 
-                'message' => $e->getMessage(),
+                'Code' => (string)$e->getCode() ?: '1000',
+                'status' =>  $MsgError[(string)$e->getCode()]['status'] ?: 'Invalid Data' ,
+                'message' => $e->getMessage()
             ));
         }
     }
