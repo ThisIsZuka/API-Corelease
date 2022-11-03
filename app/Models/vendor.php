4b92ff852e25cefac2b78463748a6a26075d4a4c
minostra <phoomin.sr@gmail.com>
2022-11-03 18:01:44 +0700

    update new field

--- app/Models/vendor.php
+++ app/Models/vendor.php
@@ -5,8 +5,62 @@ namespace App\Models;
 use App\Models\Basemodels;
 
 class vendor extends Basemodels {
+    public CONST FIELD = [
+        "VendorCode"
+      ,["ComCode"]
+      ,["VDType"]
+      ,["VDFirstName"]
+      ,['VDMiddleName']
+      ,['VDLastName']
+      ,['VDName']
+      ,['VDSearchName']
+      ,['VDLanguage']
+      ,['VDGroupCode']
+      ,['VDCouRG']
+      ,['VDHouseNo']
+      ,['VDBuilding']
+      ,['VDVillage']
+      ,['VDMoo']
+      ,['VDSoi']
+      ,['VDStreet']
+      ,['VDSubDistrict']
+      ,['VDDistrict']
+      ,['VDState']
+      ,['VDZipCode']
+      ,['VDCountry']
+      ,['VDCPhone']
+      ,['VDCFax']
+      ,['ContactName']
+      ,['CTPosition']
+      ,['CTPhone']
+      ,['VDExtension']
+      ,['CTFax']
+      ,['CTEmail']
+      ,['ResNum']
+      ,['TaxAddTypeCode']
+      ,['VDBRnumber']
+      ,['TaxCode']
+      ,['CalWHtax']
+      ,['WHTaxGroupCode']
+      ,['VDtypeWHCode']
+      ,['PaytermCode']
+      ,['VDMEPayment']
+      ,['CurCode']
+      ,['VDBankName']
+      ,['VDBankAccount']
+      ,['BankGroupCode']
+      ,['TradeGroupID']
+      ,['TradingCondition']
+      ,['Active']
+      ,['CreatedBy']
+      ,['CreatedDatetime']
+      ,['ModifiedBy']
+      ,['ModifiedDatetime']
+    ];
+
     function __construct()
     {
         parent::__construct('Purch_VendorMaster');
+        return $this->getInstant('Purch_VendorMaster');
     }
 }
